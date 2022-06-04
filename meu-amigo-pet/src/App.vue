b4d437136d07002a3bed262b27d2d5cc35935d1e
laisdeghaide <laisdeghaide@gmail.com>
2022-06-04 17:13:43 -0300

    tentando corrigir-criei um novo proj no meu-amigo-pet

---
+++ meu-amigo-pet/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Arimo&family=IBM+Plex+Mono:wght@300&family=JetBrains+Mono:wght@100;200&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <nav>
+    <HeaderView/>
+    <FooterView/>
+    <!--<router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>-->
+  </nav>
+  <router-view/>
+</template>
+
+
+<script>
+import FooterView from "./components/FooterView.vue";
+import HeaderView from "./components/HeaderView.vue";
+
+export default {
+  name: "App",
+  components: {
+    FooterView,
+    HeaderView,
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ meu-amigo-pet/src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'signup',
+    component: () => import('../views/SignUpView.vue')
+  },
+  {
+    path: '/shop-cart',
+    name: 'shopcart',
+    component: () => import('../views/ShopCartView.vue')
+  },
+  {
+    path: '/acessories',
+    name: 'acessories',
+    component: () => import('../views/AcessoriesView.vue')
+  },
+  {
+    path: '/services',
+    name: 'services',
+    component: () => import('../views/ServicesView.vue')
+  },
+  {
+    path: '/foods',
+    name: 'foods',
+    component: () => import('../views/FoodsView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
