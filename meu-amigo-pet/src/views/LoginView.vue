56bbc517a414560037f771fed5a5a8dbbf7d5d85
laisdeghaide <laisdeghaide@gmail.com>
2022-06-07 12:49:57 -0300

    iniciei cadastro

--- meu-amigo-pet/src/views/LoginView.vue
+++ meu-amigo-pet/src/views/LoginView.vue
@@ -1,4 +1,15 @@
 <template>
+    
+</template>
+
+
+
+
+
+
+
+
+<!--<template>
     <div class="container">
             <div class="sign-in-flexbox">
                 <div class="sign-in">
@@ -31,4 +42,134 @@
     justify-content: center;
 }
 
-</style>
+.sign-in h4{
+    text-align: center;
+}
+
+.sign-in h4,
+.btn_submit {
+    text-transform: uppercase;
+}
+
+input {
+    border: 2px solid #F5E9EE;
+    background-color: #FAD2E1;
+    border-radius: 5px;
+    width: 330px;
+    height: 40px;
+    padding-left: 5px;
+}
+
+input:focus {
+    border-color: #cb5c7f;
+}
+
+.formulary>div {
+    position: relative;
+    padding-top: 2px;
+}
+
+.formulary>div>label {
+    position: absolute;
+    left: 0;
+    font-size: small;
+    color: rgba(0, 0, 0, 0.4);
+    cursor: text;
+    transition: top 200ms ease-in, left 200 ease-in, font-size 200ms ease-in;
+    background-color: #FAD2E1;
+}
+
+input:focus+label,
+input:not(:placeholder-shown)+label {
+    transform: translateY(-1.8em) scale(0.9);
+    transition: 0.3s;
+    color: black;
+}
+
+input::placeholder {
+    color: transparent;
+}
+
+input:focus {
+    outline: none !important;
+}
+
+.btn_submit,
+.btn_submit:hover {
+    box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.btn_submit {
+    background-color: #EDFFBD;
+    border: 4px solid #D8FF6E;
+    border-radius: 30px;
+    width: 120px;
+    height: 50px;
+    text-align: center;
+    margin: auto;
+    display: flex;
+}
+
+
+.btn_submit[value] {
+    font-weight: 300;
+    justify-content: center;
+}
+
+.btn_submit:hover {
+    background-color: #c0eb4c;
+    border: 4px solid #c0eb4c;
+    cursor: pointer;
+}
+
+.body {
+    position: absolute;
+}
+
+.container {
+    width: 70%;
+    margin: auto auto;
+}
+
+.sign-in-flexbox{
+    display: flex;
+    flex-direction: row;
+    gap: 100px;
+}
+
+.sign-in p {
+    margin-top: 12px;
+    text-align: right;
+    text-transform: lowercase;
+    font-size: 14px;
+    padding-right: 6%;
+
+}
+
+.sign-in p a {
+    color: black;
+}
+
+.sign-in p a:hover {
+    color: rgb(50, 49, 49);
+}
+
+input {
+    margin: 10px;
+}
+
+.formulary>div>label {
+    margin: 7% 5%;
+}
+
+.pet-img {
+    width: 100%;
+    margin: auto;
+}
+
+.form-pet-logo {
+    width: 400px;
+}
+
+</style>-->
--- meu-amigo-pet/src/views/SignUpView.vue
+++ meu-amigo-pet/src/views/SignUpView.vue
@@ -1,3 +1,73 @@
 <template>
-    
+    <h1>Cadastre sua conta</h1>
+    <form>
+        <div class="col-6">
+            <label>nome</label>
+            <input type="text" required v-model="nome">
+            <label>e-mail</label>
+            <input type="email" required v-model="email">
+            <label>cpf</label>
+            <input type="text" required v-model="cpf">
+        </div>
+        <div class="col-6">
+            <label>endereço</label>
+            <input type="text" required v-model="endereco">
+            <label>telefone</label>
+            <input type="tel" required v-model="telefone">
+            <label>senha</label>
+            <input type="password" required v-model="senha">
+        </div>
+    </form>
+    <p>
 </template>
+
+<script>
+export default {
+   data() {
+       return {
+           email: '',
+           password: ''
+       }
+   }
+}
+</script>
+
+
+<style>
+h1 {
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 16px;
+    font-family: "JetBrains Mono", monospace;
+    margin-top: 15px;
+}
+
+form {
+    text-align: left;
+    margin: 0 auto;
+}
+
+label {
+    color: rgba(0, 0, 0, 0.4);
+    display: inline-block;
+    margin: 25px 0 5px;
+    font-size: 0.8em;
+    letter-spacing: 1px;
+}
+
+input {
+    display: block;
+    padding: 12px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    background-color: #FAD2E1;
+    border: 2px solid #F5E9EE;
+    border-radius: 5px;
+}
+
+.col-6 {
+    display: inline-block;
+    padding: 20px;
+    width: 420px;
+}
+</style>
