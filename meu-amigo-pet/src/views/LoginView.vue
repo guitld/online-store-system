6d0818d72dd6eeddea9de4bd71bb99d618571fe3
laisdeghaide <laisdeghaide@gmail.com>
2022-06-09 21:56:07 -0300

    cadastro finalizado o basico, mas n 100% bonito. Iniciei login

--- meu-amigo-pet/src/views/LoginView.vue
+++ meu-amigo-pet/src/views/LoginView.vue
@@ -1,175 +1,185 @@
 <template>
-    
+  <div class="container">
+    <div class="sign-in-flexbox">
+      <div class="sign-in">
+        <h4>Acesse sua conta</h4>
+        <p>
+          é novo por aqui?
+          <a href="sign_up.html"><strong>cadastre-se</strong></a>
+        </p>
+        <form>
+          <simple-input
+            v-model="event.email"
+            label="e-mail"
+            type="email"
+            required
+          ></simple-input>
+          <simple-input
+            v-model="event.senha"
+            label="senha"
+            type="password"
+            required
+          ></simple-input>
+          <input class="btn_submit" type="submit" value="ENTRAR" />
+        </form>
+      </div>
+
+      <div class="pet-img">
+        <img :src="catDogImage" alt="" />
+      </div>
+    </div>
+  </div>
 </template>
 
+<script>
+import SimpleInput from "@/components/SimpleInput.vue";
 
-
-
-
-
-
-
-<!--<template>
-    <div class="container">
-            <div class="sign-in-flexbox">
-                <div class="sign-in">
-                    <h4>Acesse sua conta</h4>
-                    <p>é novo por aqui? <a href="sign_up.html"><strong>cadastre-se</strong></a></p>
-                    <form class="formulary">
-                        <div class="email">
-                            <input type="email" id="femail" name="femail" placeholder=" " required autocomplete="off">
-                            <label>e-mail</label>
-                        </div>
-                        <div class="password">
-                            <input type="password" id="fpassword" name="fpassword" placeholder=" " required>
-                            <label>senha</label>
-                        </div>
-                        <input class="btn_submit" type="submit" value="Entrar">
-                    </form>
-                </div>
-
-                <div class="pet-img">
-                    <img src="assets/img/header-pet-na-cabine.png" alt="" class="form-pet-logo">
-                </div>
-            </div>
-        </div>
-</template>
+export default {
+  components: { SimpleInput },
+  data() {
+    return {
+      event: {
+        email: "",
+        password: "",
+      },
+      catDogImage: "assets/img/header-pet-na-cabine.png",
+    };
+  },
+};
+</script>
 
 <style scoped>
-  .container {
-    position: relative;
-    display: flex;
-    justify-content: center;
+.container {
+  position: relative;
+  display: flex;
+  justify-content: center;
 }
 
-.sign-in h4{
-    text-align: center;
+.sign-in h4 {
+  text-align: center;
 }
 
 .sign-in h4,
 .btn_submit {
-    text-transform: uppercase;
+  text-transform: uppercase;
 }
 
 input {
-    border: 2px solid #F5E9EE;
-    background-color: #FAD2E1;
-    border-radius: 5px;
-    width: 330px;
-    height: 40px;
-    padding-left: 5px;
+  border: 2px solid #f5e9ee;
+  background-color: #fad2e1;
+  border-radius: 5px;
+  width: 330px;
+  height: 40px;
+  padding-left: 5px;
 }
 
 input:focus {
-    border-color: #cb5c7f;
+  border-color: #cb5c7f;
 }
 
-.formulary>div {
-    position: relative;
-    padding-top: 2px;
+.formulary > div {
+  position: relative;
+  padding-top: 2px;
 }
 
-.formulary>div>label {
-    position: absolute;
-    left: 0;
-    font-size: small;
-    color: rgba(0, 0, 0, 0.4);
-    cursor: text;
-    transition: top 200ms ease-in, left 200 ease-in, font-size 200ms ease-in;
-    background-color: #FAD2E1;
+.formulary > div > label {
+  position: absolute;
+  left: 0;
+  font-size: small;
+  color: rgba(0, 0, 0, 0.4);
+  cursor: text;
+  transition: top 200ms ease-in, left 200 ease-in, font-size 200ms ease-in;
+  background-color: #fad2e1;
 }
 
-input:focus+label,
-input:not(:placeholder-shown)+label {
-    transform: translateY(-1.8em) scale(0.9);
-    transition: 0.3s;
-    color: black;
+input:focus + label,
+input:not(:placeholder-shown) + label {
+  transform: translateY(-1.8em) scale(0.9);
+  transition: 0.3s;
+  color: black;
 }
 
 input::placeholder {
-    color: transparent;
+  color: transparent;
 }
 
 input:focus {
-    outline: none !important;
+  outline: none !important;
 }
 
 .btn_submit,
 .btn_submit:hover {
-    box-sizing: border-box;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
 .btn_submit {
-    background-color: #EDFFBD;
-    border: 4px solid #D8FF6E;
-    border-radius: 30px;
-    width: 120px;
-    height: 50px;
-    text-align: center;
-    margin: auto;
-    display: flex;
+  background-color: #edffbd;
+  border: 4px solid #d8ff6e;
+  border-radius: 30px;
+  width: 120px;
+  height: 50px;
+  text-align: center;
+  margin: auto;
+  display: flex;
 }
 
-
 .btn_submit[value] {
-    font-weight: 300;
-    justify-content: center;
+  font-weight: 300;
+  justify-content: center;
 }
 
 .btn_submit:hover {
-    background-color: #c0eb4c;
-    border: 4px solid #c0eb4c;
-    cursor: pointer;
+  background-color: #c0eb4c;
+  border: 4px solid #c0eb4c;
+  cursor: pointer;
 }
 
 .body {
-    position: absolute;
+  position: absolute;
 }
 
 .container {
-    width: 70%;
-    margin: auto auto;
+  width: 70%;
+  margin: auto auto;
 }
 
-.sign-in-flexbox{
-    display: flex;
-    flex-direction: row;
-    gap: 100px;
+.sign-in-flexbox {
+  display: flex;
+  flex-direction: row;
+  gap: 100px;
 }
 
 .sign-in p {
-    margin-top: 12px;
-    text-align: right;
-    text-transform: lowercase;
-    font-size: 14px;
-    padding-right: 6%;
-
+  margin-top: 12px;
+  text-align: right;
+  text-transform: lowercase;
+  font-size: 14px;
+  padding-right: 6%;
 }
 
 .sign-in p a {
-    color: black;
+  color: black;
 }
 
 .sign-in p a:hover {
-    color: rgb(50, 49, 49);
+  color: rgb(50, 49, 49);
 }
 
 input {
-    margin: 10px;
+  margin: 10px;
 }
 
-.formulary>div>label {
-    margin: 7% 5%;
+.formulary > div > label {
+  margin: 7% 5%;
 }
 
 .pet-img {
-    width: 100%;
-    margin: auto;
+  width: 100%;
+  margin: auto;
 }
 
 .form-pet-logo {
-    width: 400px;
+  width: 400px;
 }
-
-</style>-->
+</style>
--- meu-amigo-pet/src/views/SignUpView.vue
+++ meu-amigo-pet/src/views/SignUpView.vue
@@ -1,73 +1,124 @@
 <template>
-    <h1>Cadastre sua conta</h1>
-    <form>
-        <div class="col-6">
-            <label>nome</label>
-            <input type="text" required v-model="nome">
-            <label>e-mail</label>
-            <input type="email" required v-model="email">
-            <label>cpf</label>
-            <input type="text" required v-model="cpf">
-        </div>
-        <div class="col-6">
-            <label>endereço</label>
-            <input type="text" required v-model="endereco">
-            <label>telefone</label>
-            <input type="tel" required v-model="telefone">
-            <label>senha</label>
-            <input type="password" required v-model="senha">
-        </div>
-    </form>
-    <p>
+  <h1>Cadastre sua conta</h1>
+  <form>
+    <simple-input
+      v-model="event.nome" label="nome" type="text" required
+    ></simple-input>
+    <simple-input
+      v-model="event.email" label="e-mail" type="email" required
+    ></simple-input>
+    <simple-input
+      v-model="event.cpf" label="cpf" type="text" pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})" required
+    ></simple-input>
+    <simple-input
+      v-model="event.endereco" label="endereço" type="text" required
+    ></simple-input>
+    <simple-input
+      v-model="event.telefone" label="telefone" type="tel" required
+    ></simple-input>
+    <simple-input
+      v-model="event.senha" label="senha" type="password" required
+    ></simple-input>
+    <input
+      class="btn_submit"
+      type="submit"
+      value="CADASTRAR"
+      @click="cadastrar()"
+    />
+  </form>
 </template>
 
 <script>
+import SimpleInput from "@/components/SimpleInput.vue";
 export default {
-   data() {
-       return {
-           email: '',
-           password: ''
-       }
-   }
-}
+  components: { SimpleInput },
+  data() {
+    return {
+      event: {
+        nome: "",
+        email: "",
+        cpf: "",
+        endereco: "",
+        telefone: "",
+        senha: "",
+      },
+    };
+  },
+  methods: {
+    cadastrar() {
+      // REFINAR ISSO AQUI, QUANDO CLICA, DÁ O ALERTA MESMO N TENDO CAMPO PREENCHIDO
+      alert("Cadastro realizado com sucesso!");
+    },
+  },
+};
 </script>
 
 
 <style>
-h1 {
-    text-transform: uppercase;
-    text-align: center;
-    font-size: 16px;
+h1, input {
     font-family: "JetBrains Mono", monospace;
-    margin-top: 15px;
+}
+
+h1 {
+  text-transform: uppercase;
+  text-align: center;
+  font-size: 16px;
+  margin-top: 15px;
 }
 
 form {
-    text-align: left;
-    margin: 0 auto;
+  margin: 0 auto;
 }
 
-label {
-    color: rgba(0, 0, 0, 0.4);
-    display: inline-block;
-    margin: 25px 0 5px;
-    font-size: 0.8em;
-    letter-spacing: 1px;
-}
 
 input {
-    display: block;
-    padding: 12px 6px;
-    width: 100%;
-    box-sizing: border-box;
-    background-color: #FAD2E1;
-    border: 2px solid #F5E9EE;
-    border-radius: 5px;
+  display: block;
+
+  width: 350px;
+  padding: 10px 5px;
+  margin: 20px;
+
+  font-size: 0.75em;
+  letter-spacing: 1px;
+  color: rgba(0, 0, 0, 0.7);
+
+  box-sizing: border-box;
+  background-color: #fad2e1;
+  border: 2px solid #f5e9ee;
+  border-radius: 5px;
 }
 
-.col-6 {
-    display: inline-block;
-    padding: 20px;
-    width: 420px;
+input:focus {
+  border-color: #cb5c7f;
+  outline: none !important;
 }
+
+.btn_submit,
+.btn_submit:hover {
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.btn_submit {
+  background-color: #edffbd;
+  border: 4px solid #d8ff6e;
+  border-radius: 30px;
+  width: 120px;
+  height: 50px;
+  text-align: center;
+  margin: 10px auto;
+  display: flex;
+}
+
+.btn_submit[value] {
+  font-weight: 300;
+  justify-content: center;
+}
+
+.btn_submit:hover {
+  background-color: #c0eb4c;
+  border: 4px solid #c0eb4c;
+  cursor: pointer;
+}
+
 </style>
